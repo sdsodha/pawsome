4b764045497b73a5698de0174efc0f4c89a9d0a9
dionyxus <asingh309@mylangara.ca>
2023-06-20 20:46:44 -0700

    Pet Select Network run test

--- Frontend/src/components/login/PetSelectScreen.js
+++ Frontend/src/components/login/PetSelectScreen.js
@@ -7,103 +7,104 @@ import { Select } from 'native-base';
 
 
 const PetSelectScreen = () => {
-  const navigation = useNavigation()
-  const [difficulty, setDifficulty] = useState('');
-  const [name, setName] = useState('');
-  const [breed, setBreed] = useState('');
-  const [sex, setSex] = useState('');
-  const [selectedPicture, setSelectedPicture] = useState('');
+    const navigation = useNavigation()
+    const [difficulty, setDifficulty] = useState('');
+    const [name, setName] = useState('');
+    const [breed, setBreed] = useState('');
+    const [sex, setSex] = useState('');
+    const [selectedPicture, setSelectedPicture] = useState('');
 
-  const [currentUserId, setCurrentUserId] = useState('');
+    const [currentUserId, setCurrentUserId] = useState('');
 
-  const width = Dimensions.get('window').width;
-  const picList = [];
-  picList.push('../../../assets/picture1.jpg');
-  picList.push('../../../assets/picture2.jpg');
-  picList.push('../../../assets/picture3.jpg');
-  picList.push('../../../assets/picture4.jpg');
-  picList.push('../../../assets/picture5.jpg');
+    const width = Dimensions.get('window').width;
+    const picList = [];
+    picList.push('../../../assets/picture1.jpg');
+    picList.push('../../../assets/picture2.jpg');
+    picList.push('../../../assets/picture3.jpg');
+    picList.push('../../../assets/picture4.jpg');
+    picList.push('../../../assets/picture5.jpg');
 
-  const data = [
-    { color: 'red' },
-    { color: 'purple' },
-    { color: 'blue' },
-    { color: 'yellow' },
-    { color: 'green' },
-    { color: 'pink' },
-    { color: 'white' },
-  ];
+    const data = [
+        { color: 'red' },
+        { color: 'purple' },
+        { color: 'blue' },
+        { color: 'yellow' },
+        { color: 'green' },
+        { color: 'pink' },
+        { color: 'white' },
+    ];
 
 
-  useEffect(() => {
+    useEffect(() => {
 
-    //  fetchCurrentUserId();
-  }, []);
+        fetchCurrentUserId();
+    }, []);
 
-  const fetchCurrentUserId = async () => {
-    try {
-      const currentUser = auth.currentUser;
-      if (currentUser) {
-        const response = await fetch('http://localhost:8080/users');
-        const data = await response.json();
-        const foundUser = data.find((user) => user.uid === currentUser.uid);
-        if (foundUser) {
-          setCurrentUserId(foundUser._id);
-        } else {
-          console.log('User not found in the server data');
+    const fetchCurrentUserId = async () => {
+        try {
+            const currentUser = auth.currentUser;
+            if (currentUser) {
+                const response = await fetch('http://localhost:8080/users');
+                const data = await response.json();
+                const foundUser = data.find((user) => user.uid === currentUser.uid);
+                if (foundUser) {
+                    setCurrentUserId(foundUser._id);
+                    console.log("User Found", foundUser._id);
+                } else {
+                    console.log('User not found in the server data');
+                }
+            } else {
+                console.log('User is not logged in');
+            }
+        } catch (error) {
+            console.error(error);
         }
-      } else {
-        console.log('User is not logged in');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleSubmit = (currentUserId) => {
-    // Perform form submission logic here
-    // You can access the selected values using the state variables
-    const formData = {
-      user: currentUserId,
-      difficulty: difficulty,
-      name: name,
-      breed: breed,
-      sex: sex,
-      selectedPicture: selectedPicture,
     };
 
-    console.log(formData);
+    const handleSubmit = (currentUserId) => {
+        // Perform form submission logic here
+        // You can access the selected values using the state variables
+        const formData = {
+            user: currentUserId,
+            difficulty: difficulty,
+            name: name,
+            breed: breed,
+            sex: sex,
+            selectedPicture: selectedPicture,
+        };
 
-    fetch(`http://localhost:8080/users/pet-form`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response data
-        console.log(data);
-      })
-      .catch((error) => {
-        // Handle any error that occurred during the request
-        console.error(error);
-      });
-    // Clear the form fields after submission
-    Alert.alert('Form Saved', 'The form has been successfully saved.');
-    setDifficulty('');
-    setName('');
-    setBreed('');
-    setSex('');
-    setSelectedPicture('');
-    navigation.navigate("Home")
-  };
+        console.log(formData);
+
+        fetch(`http://localhost:8080/users/pet-form`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Handle the response data
+                console.log(data);
+            })
+            .catch((error) => {
+                // Handle any error that occurred during the request
+                console.error(error);
+            });
+        // Clear the form fields after submission
+        Alert.alert('Form Saved', 'The form has been successfully saved.');
+        setDifficulty('');
+        setName('');
+        setBreed('');
+        setSex('');
+        setSelectedPicture('');
+        navigation.navigate("Home")
+    };
 
 
-  return (
-    <View style={styles.container}>
-      {/* 
+    return (
+        <View style={styles.container}>
+            {/* 
     <Text>Welcome to the Pet Form</Text>
       <Button
         title="Go to Pet Screen"
@@ -120,81 +121,81 @@ const PetSelectScreen = () => {
     */}
 
 
-      <Text style={styles.title}>Select Your Pet</Text>
+            <Text style={styles.title}>Select Your Pet</Text>
 
 
-      <View style={styles.fieldContainer}>
-        <Text style={styles.label}>Difficulty Level:</Text>
-        <View style={styles.picker}>
-          <Select
-            selectedValue={difficulty}
-            onValueChange={(value) => setDifficulty(value)}
-          >
-            <Select.Item label="Select Difficulty" value="" />
-            <Select.Item label="Easy" value="easy" />
-            <Select.Item label="Medium" value="medium" />
-            <Select.Item label="Hard" value="hard" />
-          </Select>
-        </View>
-      </View>
+            <View style={styles.fieldContainer}>
+                <Text style={styles.label}>Difficulty Level:</Text>
+                <View style={styles.picker}>
+                    <Select
+                        selectedValue={difficulty}
+                        onValueChange={(value) => setDifficulty(value)}
+                    >
+                        <Select.Item label="Select Difficulty" value="" />
+                        <Select.Item label="Easy" value="easy" />
+                        <Select.Item label="Medium" value="medium" />
+                        <Select.Item label="Hard" value="hard" />
+                    </Select>
+                </View>
+            </View>
 
-      <Text style={styles.label}>Pictures:</Text>
-      <View style={styles.pictureContainer}>
-        <View style={styles.pictureItem}>
-          <Image
-            source={require('../../../assets/picture1.jpg')}
-            style={styles.picture}
-          />
-          <Text style={styles.pictureLabel}>Picture 1</Text>
-        </View>
-        <View style={styles.pictureItem}>
-          <Image
-            source={require('../../../assets/picture2.jpg')}
-            style={styles.picture}
-          />
-          <Text style={styles.pictureLabel}>Picture 2</Text>
-        </View>
-        <View style={styles.pictureItem}>
-          <Image
-            source={require('../../../assets/picture3.jpg')}
-            style={styles.picture}
-          />
-          <Text style={styles.pictureLabel}>Picture 3</Text>
-        </View>
-        <View style={styles.pictureItem}>
-          <Image
-            source={require('../../../assets/picture4.jpg')}
-            style={styles.picture}
-          />
-          <Text style={styles.pictureLabel}>Picture 4</Text>
-        </View>
-        <View style={styles.pictureItem}>
-          <Image
-            source={require('../../../assets/picture5.jpg')}
-            style={styles.picture}
-          />
-          <Text style={styles.pictureLabel}>Picture 5</Text>
-        </View>
-      </View>
+            <Text style={styles.label}>Pictures:</Text>
+            <View style={styles.pictureContainer}>
+                <View style={styles.pictureItem}>
+                    <Image
+                        source={require('../../../assets/picture1.jpg')}
+                        style={styles.picture}
+                    />
+                    <Text style={styles.pictureLabel}>Picture 1</Text>
+                </View>
+                <View style={styles.pictureItem}>
+                    <Image
+                        source={require('../../../assets/picture2.jpg')}
+                        style={styles.picture}
+                    />
+                    <Text style={styles.pictureLabel}>Picture 2</Text>
+                </View>
+                <View style={styles.pictureItem}>
+                    <Image
+                        source={require('../../../assets/picture3.jpg')}
+                        style={styles.picture}
+                    />
+                    <Text style={styles.pictureLabel}>Picture 3</Text>
+                </View>
+                <View style={styles.pictureItem}>
+                    <Image
+                        source={require('../../../assets/picture4.jpg')}
+                        style={styles.picture}
+                    />
+                    <Text style={styles.pictureLabel}>Picture 4</Text>
+                </View>
+                <View style={styles.pictureItem}>
+                    <Image
+                        source={require('../../../assets/picture5.jpg')}
+                        style={styles.picture}
+                    />
+                    <Text style={styles.pictureLabel}>Picture 5</Text>
+                </View>
+            </View>
 
-      <View style={styles.fieldContainer}>
-        <Text style={styles.label}>Picture:</Text>
-        <View style={styles.picker}>
-          <Select
-            selectedValue={selectedPicture}
-            onValueChange={(value) => setSelectedPicture(value)}
-          >
-            <Select.Item label="Select Picture" value="" />
-            <Select.Item label="Picture 1" value="picture1" />
-            <Select.Item label="Picture 2" value="picture2" />
-            <Select.Item label="Picture 3" value="picture3" />
-            <Select.Item label="Picture 4" value="picture4" />
-            <Select.Item label="Picture 5" value="picture5" />
-          </Select>
-        </View>
-      </View>
+            <View style={styles.fieldContainer}>
+                <Text style={styles.label}>Picture:</Text>
+                <View style={styles.picker}>
+                    <Select
+                        selectedValue={selectedPicture}
+                        onValueChange={(value) => setSelectedPicture(value)}
+                    >
+                        <Select.Item label="Select Picture" value="" />
+                        <Select.Item label="Picture 1" value="picture1" />
+                        <Select.Item label="Picture 2" value="picture2" />
+                        <Select.Item label="Picture 3" value="picture3" />
+                        <Select.Item label="Picture 4" value="picture4" />
+                        <Select.Item label="Picture 5" value="picture5" />
+                    </Select>
+                </View>
+            </View>
 
-      {/* 
+            {/* 
       <View style={{ flex: 1 }}>
         <Carousel
           width={width}
@@ -218,94 +219,94 @@ const PetSelectScreen = () => {
         />
       </View>
  */}
-      <View style={styles.fieldContainer}>
-        <Text style={styles.label}>Name:</Text>
-        <TextInput style={styles.input} value={name} onChangeText={(text) => setName(text)} />
-      </View>
+            <View style={styles.fieldContainer}>
+                <Text style={styles.label}>Name:</Text>
+                <TextInput style={styles.input} value={name} onChangeText={(text) => setName(text)} />
+            </View>
 
-      <View style={styles.fieldContainer}>
-        <Text style={styles.label}>Breed:</Text>
-        <TextInput style={styles.input} value={breed} onChangeText={(text) => setBreed(text)} />
-      </View>
+            <View style={styles.fieldContainer}>
+                <Text style={styles.label}>Breed:</Text>
+                <TextInput style={styles.input} value={breed} onChangeText={(text) => setBreed(text)} />
+            </View>
 
-      <View style={styles.fieldContainer}>
-        <Text style={styles.label}>Sex:</Text>
-        <View style={styles.picker}>
-          <Select
-            selectedValue={sex}
-            onValueChange={(value) => setSex(value)}
-          >
-            <Select.Item label="Select value" value="" />
-            <Select.Item label="Male" value="male" />
-            <Select.Item label="Female" value="female" />
-          </Select>
+            <View style={styles.fieldContainer}>
+                <Text style={styles.label}>Sex:</Text>
+                <View style={styles.picker}>
+                    <Select
+                        selectedValue={sex}
+                        onValueChange={(value) => setSex(value)}
+                    >
+                        <Select.Item label="Select value" value="" />
+                        <Select.Item label="Male" value="male" />
+                        <Select.Item label="Female" value="female" />
+                    </Select>
+                </View>
+            </View>
+
+
+            <Button title="Submit" onPress={() => handleSubmit(currentUserId)} />
         </View>
-      </View>
-
-
-      <Button title="Submit" onPress={() => handleSubmit(currentUserId)} />
-    </View>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  fieldContainer: {
-    marginBottom: 16,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    paddingVertical: 8,
-    paddingHorizontal: 12,
-  },
-  picker: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-  },
-  pictureContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-  },
-  pictureItem: {
-    width: '20%',
-    marginBottom: 16,
-    alignItems: 'center',
-  },
-  picture: {
-    width: 100,
-    height: 100,
-  },
-  pictureLabel: {
-    marginTop: 8,
-  },
-  button: {
-    backgroundColor: '#0782F9',
-    width: '60%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    marginTop: 40,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    fieldContainer: {
+        marginBottom: 16,
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 8,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+    },
+    picker: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+    },
+    pictureContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
+    pictureItem: {
+        width: '20%',
+        marginBottom: 16,
+        alignItems: 'center',
+    },
+    picture: {
+        width: 100,
+        height: 100,
+    },
+    pictureLabel: {
+        marginTop: 8,
+    },
+    button: {
+        backgroundColor: '#0782F9',
+        width: '60%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+    },
 });
 
 export default PetSelectScreen;
